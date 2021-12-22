2f5d01201c085625eb4ca7da8a0da32ff96fc26b
phunky-panda94 <victor.nim@94outlook.com>
2021-12-22 20:23:40 +1100

    inserted middleware functions into routes, to be tested

--- server/src/user/user.routes.js
+++ server/src/user/user.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('./user.controller');
+const { authenticateToken, authenticateUser } = require('../auth/auth');
 
-router.get('/:id', userController.getUser);
+router.get('/:id', authenticateToken, userController.getUser);
 
 router.post('/register', userController.register);
 
-router.post('/login', userController.login);
+router.post('/login', authenticateUser ,userController.login);
 
 module.exports = router;
