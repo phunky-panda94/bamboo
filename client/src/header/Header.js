da431d6acb141e6e17e27b9b9c2d86827a01d33d
phunky-panda94 <victor.nim@94outlook.com>
2022-01-05 15:15:56 +1100

    added brand to home button

--- client/src/header/Header.js
+++ client/src/header/Header.js
@@ -3,11 +3,12 @@ import './Header.css';
 function Navbar() {
     return (
         <header className="bg-light-black navbar flex flex-jc-sb flex-ai-c">
-            <a className="flex flex-ai-c" href="/">
-                <img alt="panda" src="/logo.png" className="logo"></img>
+            <a className="home flex flex-ai-c" href="/">
+                <img alt="panda" src="/panda.png" className="logo"></img>
+                <img alt="bamboo" src="/bamboo.png" className="brand"></img>
             </a>
             <div className="flex flex-ai-c">
-                <button className="dark-green navbar-btn">Log In</button>
+                <button className="bg-white dark-green navbar-btn">Log In</button>
                 <button className="bg-dark-green white navbar-btn">Sign Up</button>
                 <button className="profile">
                     <span className="white material-icons-outlined">person</span>
