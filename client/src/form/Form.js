7fd48baff21c355fc301b212b4882296d4f4b246
phunky-panda94 <victor.nim@94outlook.com>
2022-01-06 14:49:00 +1100

    styled login form

--- client/src/form/Form.js
+++ client/src/form/Form.js
@@ -8,17 +8,21 @@ function Form() {
                 <div>
                     <img src="/tree.png"></img>
                 </div>
-                <form className="form-field-group flex flex-col flex-jc-c">
-                    <h3>Login</h3>
-                    <Field name="email" label="Email"/>
-                    <Field name="password" label="Password" />
-                    <button type="submit">Log In</button>
-                    <span className="small-font">
-                        Forgot your <a className="dark-green">username</a> or <a className="dark-green">password</a>?
-                    </span>
-                </form>
-                <div className="border">
-                    <span className="grey close">&times;</span>
+                <div className="form-container flex flex-col">
+                    <div className="flex flex-jc-fe">
+                        <span className="grey close">&times;</span>
+                    </div>
+                    <div className="form-container flex flex-jc-c flex-ai-c">
+                        <form className="form-field-group flex flex-col">
+                            <h3>Login</h3>
+                            <Field name="email" label="Email"/>
+                            <Field name="password" label="Password" />
+                            <button type="submit">Log In</button>
+                            <span className="small-font">
+                                Forgot your <a className="dark-green">username</a> or <a className="dark-green">password</a>?
+                            </span>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
