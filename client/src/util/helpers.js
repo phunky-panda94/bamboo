f00badc6681c8f062930be24fdf7d798b28c06ca
phunky-panda94 <victor.nim@94outlook.com>
2022-01-10 18:51:15 +1100

    refactored getTimeElapsed function

--- client/src/util/helpers.js
+++ client/src/util/helpers.js
@@ -1,57 +1,56 @@
 export const getTimeElapsed = (timestamp, now) => {
 
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+    const month = day * 30;
+    const year = month * 12; 
+
     let currentDate = new Date(now);
     let date = new Date(timestamp);
-    let timeElapsed = (currentDate - date) / (1000 * 60); // minutes
-
-    if (timeElapsed < 1) {
+    let timeElapsed = currentDate - date
+    
+    if (timeElapsed < minute) {
         return 'less than a minute ago';
     }
-    
-    if (Math.round(timeElapsed) == 1) {
-        return '1 minute ago';
-    } 
 
-    if (timeElapsed > 1 && timeElapsed < 59) {
-        return `${Math.round(timeElapsed)} minutes ago`;
+    if (timeElapsed >= minute && timeElapsed < hour) {
+        timeElapsed = Math.round(timeElapsed / minute);
+        if (timeElapsed == 1) {
+            return '1 minute ago';
+        }
+        return `${timeElapsed} minutes ago`;
     }
 
-    if (timeElapsed == 60) {
-        return `1 hour ago`;
+    if (timeElapsed >= hour && timeElapsed < day) {
+        timeElapsed = Math.round(timeElapsed / hour);
+        if (timeElapsed == 1) {
+            return '1 hour ago';
+        }
+        return `${timeElapsed} hours ago`;
     }
 
-    timeElapsed /= 60 // convert to hours
-
-    if (timeElapsed < 24) {
-        return `${Math.round(timeElapsed)} hours ago`;
+    if (timeElapsed >= day && timeElapsed < month) {
+        timeElapsed = Math.round(timeElapsed / day);
+        if (timeElapsed == 1) {
+            return '1 day ago';
+        }
+        return `${timeElapsed} days ago`;
     }
 
-    if (Math.round(timeElapsed) == 24) {
-        return `1 day ago`;
+    if (timeElapsed >= month && timeElapsed < year) {
+        timeElapsed = Math.round(timeElapsed / month);
+        if (timeElapsed == 1) {
+            return '1 month ago';
+        }
+        return `${timeElapsed} months ago`;
     }
 
-    timeElapsed /= 24 // convert to days
-
-    if (timeElapsed < 30) {
-        return `${Math.round(timeElapsed)} days ago`;
-    }
-
-    if (timeElapsed == 30) {
-        return '1 month ago';
-    }
-
-    timeElapsed /= 30 // convert to months
-
-    if (timeElapsed < 12) {
-        return `${Math.round(timeElapsed)} months ago`;
-    }
-
-    if (timeElapsed == 12) {
+    timeElapsed = Math.round(timeElapsed / year);
+    if (timeElapsed == 1) {
         return '1 year ago';
     }
-
-    timeElapsed /= 12 // convert to years
-
-    return `${Math.round(timeElapsed)} years ago`; 
+    return `${timeElapsed} years ago`; 
 
 }
